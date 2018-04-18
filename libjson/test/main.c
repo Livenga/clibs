880d25b865515360fc43faa661cdad69bdb051f7
Livenga <ljmmz0405@gmail.com>
2018-04-18 14:44:53 +0900

    test

--- libjson/test/main.c
+++ libjson/test/main.c
@@ -7,24 +7,27 @@
 
 int
 main(int argc, char *argv[]) {
-  struct json_object *jo;
-  long l = 132L;
-  double f = 3.14;
+  char *buf;
+  struct json_object *object, *in_object;
 
-  const char *hello = "Hello, World!";
+  object    = json_object_new(NULL);
+  in_object = json_object_new("in_object_1");
 
+  json_add_member(object, (struct json_common *)json_string_new("key1", "value1"));
+  json_add_member(object, (struct json_common *)json_string_new("key2", "value2"));
+  json_add_member(object, (struct json_common *)json_string_new("key3", "value3"));
+  json_add_member(object, (struct json_common *)json_string_new("key4", "value4"));
+  json_add_member(object, (struct json_common *)json_string_new("key5", "value5"));
 
-  jo = json_object_new(NULL);
+  json_add_member(object, (struct json_common *)in_object);
+  json_add_member(in_object, (struct json_common *)json_number_new("number1", 1204));
 
-  jo->member = json_value_new(NUMBER, 1, "value", (void *)&l);
-  jo->member->add(jo->member, json_value_new(STRING, strlen(hello), "string", (void *)hello));
+  // 文字列変換
+  buf = json_to_string((struct json_common *)object);
+  printf("%s\n", buf);
+  free((void *)buf); buf = NULL;
 
-
-  printf("%ld\n", json_count(jo));
-  json_print(jo);
-
-  json_object_free(jo);
-  printf("%ld\n", json_count(jo));
+  json_object_free(object);
 
   return 0;
 }
